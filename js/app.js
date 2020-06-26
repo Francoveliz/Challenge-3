68baa9b8d4125b1869dbedaf9f08301b35b9a210
Francoveliz <francoleandroveliz@gmail.com>
2020-06-26 04:19:21 -0300

    Initial commit

--- js/app.js
+++ js/app.js
@@ -1,55 +1,16 @@
-import barba from '@barba/core';
-import css from '@barba/css';
-
-// tell Barba to use the css plugin
-barba.use(css);
-// init Barba
-
 barba.init({
   transitions: [{
-    transitions: [{
-    name: 'slide-up-',
-    leave() {},
-    enter() {},
-    from: {
-      namespace: 'tanya'
+    name: 'opacity-transition',
+    sync:true,
+    leave(data) {
+      return gsap.to(data.current.container, {
+        opacity: 0
+      });
     },
-    to: {
-      namespace: 'john'
-    }
-  }, {
-    name: 'slide-down-',
-    leave() {},
-    enter() {},
-    from: {
-      namespace: 'john'
-    },
-    to: {
-      namespace: 'tanya'
-    }
-  }, {
-    name: 'slide-left-',
-    leave() {},
-    enter() {},
-    from: {
-      custom: ({ trigger }) => {
-        return trigger.classList && trigger.classList.contains('direction--prev');
-      }
-    },
-    to: {
-      namespace: 'product'
-    }
-  }, {
-    name: 'slide-right-',
-    leave() {},
-    enter() {},
-    from: {
-      custom: ({ trigger }) => {
-        return trigger.classList && trigger.classList.contains('direction--next');
-      }
-    },
-    to: {
-      namespace: ''
+    enter(data) {
+      return gsap.from(data.next.container, {
+        opacity: 0
+      });
     }
   }]
 });
